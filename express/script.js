ac99857f823c1e7abb6cbdf2d20ae1b54d5b8ef0
harishmeena31 <harishmbmit.com>
2023-11-23 14:00:01 +0530

    Your commit message here

--- express/script.js
+++ express/script.js
@@ -2,21 +2,21 @@ const express = require('express')
 const app = express();
 
 app.use(function(req, res, next){
-  next();
-});
+    console.log("middleware working");
+    next();  
+ });
 
 app.use(function(req, res, next){
-  next();
-});
-
-
+    console.log("middleware 2 working");
+    next();  
+ });
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.get('/profile', function (req, res) {
-  res.send('Hello World profile')
+  res.send('Hello World profile hjklln ghvhgvhg')
 })
 
 app.listen(3000)
