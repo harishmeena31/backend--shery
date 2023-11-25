578fde2584b2d5b4a101858debe90e95e493d929
harishmeena31 <harishmbmit.com>
2023-11-25 15:56:25 +0530

    new commit

--- scrpit.js
+++ scrpit.js
@@ -1,14 +1,15 @@
-const express = require('express')
+ const express = require('express')
 const app = express()
 
 app.set ("view engine", "ejs");
 app.use(express.static('./public'))
 
-app.get('/', function (req, res) {
-  res.render("index");
+app.get('/', function (req, res, next) {
+  // res.render("index");
+  throw Error ("i don't know")
 })
 
-app.get('/error', function (req, res, next) {
+app.get('/error', function (req, res) {
   throw Error ("something went wrong")
 })
 
@@ -16,11 +17,12 @@ app.get('/contact', function (req, res) {
   res.render("contact", {name :"Harish"},);
 })
 
-function errorHandler (err, req, res, next) {
+app.use(function errorHandler (err, req, res, next) {
   if (res.headersSent) {
     return next(err)
   }
   res.status(500)
   res.render('error', { error: err })
-}
+});
+
 app.listen(3000)
